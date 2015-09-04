74c6c64ca0bd9302fe419b4e3728a84c83d151dd
Jeremy Hooke <jez@stulk.com>
2015-09-04 12:32:33 +1000

    Add missing band definitions (off-by-one error on generator)

--- database/sql/v7__usgs_sr.sql
+++ database/sql/v7__usgs_sr.sql
@@ -49,7 +49,10 @@ insert into processing_level (level_id, level_name, nodata_value, resampling_met
 -- Example: 'CONUS_3.2_1.0_LE70470262013359EDC00_cfmask.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (601, 3, 'usgs cfmask', 1, 1, 30, null, null, 'LE7.*_USGS_CFMASK.*', 2, 'USGS_CFMASK', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (602, 3, 'usgs cfmask conf', 1, 1, 30, null, null, 'LE7.*_USGS_CFMASK.*', 2, 'USGS_CFMASK_CONF', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 601, 73, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 602, 73, 2);
 -- Example: 'CONUS_3.2_1.0_LE70470262013359EDC00_sr_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (611, 3, 'usgs sr adjacent cloud qa', 1, 2, 30, null, null, 'LE7.*_USGS_SR_ATTR.*', 2, 'USGS_SR_ADJACENT_CLOUD_QA', 1);
@@ -63,12 +66,15 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (615, 3, 'usgs sr fill qa', 1, 2, 30, null, null, 'LE7.*_USGS_SR_ATTR.*', 2, 'USGS_SR_FILL_QA', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (616, 3, 'usgs sr land water qa', 1, 2, 30, null, null, 'LE7.*_USGS_SR_ATTR.*', 2, 'USGS_SR_LAND_WATER_QA', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (617, 3, 'usgs sr snow qa', 1, 2, 30, null, null, 'LE7.*_USGS_SR_ATTR.*', 2, 'USGS_SR_SNOW_QA', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 611, 71, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 612, 71, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 613, 71, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 614, 71, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 615, 71, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 616, 71, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 617, 71, 7);
 -- Example: 'CONUS_3.2_1.0_LE70470262013359EDC00_sr_band.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (621, 3, 'usgs sr atmos opacity', 1, 3, 30, null, null, 'LE7.*_USGS_SR_BAND.*', 2, 'USGS_SR_ATMOS_OPACITY', 1);
@@ -82,16 +88,22 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (625, 3, 'usgs sr band4', 1, 3, 30, null, null, 'LE7.*_USGS_SR_BAND.*', 2, 'USGS_SR_BAND4', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (626, 3, 'usgs sr band5', 1, 3, 30, null, null, 'LE7.*_USGS_SR_BAND.*', 2, 'USGS_SR_BAND5', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (627, 3, 'usgs sr band7', 1, 3, 30, null, null, 'LE7.*_USGS_SR_BAND.*', 2, 'USGS_SR_BAND7', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 621, 70, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 622, 70, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 623, 70, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 624, 70, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 625, 70, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 626, 70, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 627, 70, 7);
 -- Example: 'CONUS_3.2_1.0_LE70470262013359EDC00_sr_int16_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (631, 3, 'usgs sr evi', 1, 4, 30, null, null, 'LE7.*_USGS_SR_INT16_ATTR.*', 2, 'USGS_SR_EVI', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (632, 3, 'usgs sr ndvi', 1, 4, 30, null, null, 'LE7.*_USGS_SR_INT16_ATTR.*', 2, 'USGS_SR_NDVI', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 631, 72, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 632, 72, 2);
 
 
 -- Landsat 8 OLI_TIRS
@@ -99,8 +111,14 @@ insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6,
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_cfmask.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (641, 4, 'usgs cfmask', 1, 5, 30, null, null, 'L.8.*_USGS_CFMASK.*', 3, 'USGS_CFMASK', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (642, 4, 'usgs cfmask conf', 1, 5, 30, null, null, 'L.8.*_USGS_CFMASK.*', 3, 'USGS_CFMASK_CONF', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 641, 73, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 642, 73, 2);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_attr.tif'
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (651, 4, 'usgs sr cloud', 1, 6, 30, null, null, 'L.8.*_USGS_SR_ATTR.*', 3, 'USGS_SR_CLOUD', 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 651, 71, 1);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_band.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (661, 4, 'usgs sr band1', 1, 7, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND1', 1);
@@ -114,16 +132,22 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (665, 4, 'usgs sr band5', 1, 7, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND5', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (666, 4, 'usgs sr band6', 1, 7, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND6', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (667, 4, 'usgs sr band7', 1, 7, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND7', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 661, 70, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 662, 70, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 663, 70, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 664, 70, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 665, 70, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 666, 70, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 667, 70, 7);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_int16_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (671, 4, 'usgs sr evi', 1, 8, 30, null, null, 'L.8.*_USGS_SR_INT16_ATTR.*', 3, 'USGS_SR_EVI', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (672, 4, 'usgs sr ndvi', 1, 8, 30, null, null, 'L.8.*_USGS_SR_INT16_ATTR.*', 3, 'USGS_SR_NDVI', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 671, 72, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 672, 72, 2);
 
 
 -- Landsat 8 OLI
@@ -131,8 +155,14 @@ insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6,
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_cfmask.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (681, 6, 'usgs cfmask', 1, 9, 30, null, null, 'L.8.*_USGS_CFMASK.*', 3, 'USGS_CFMASK', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (682, 6, 'usgs cfmask conf', 1, 9, 30, null, null, 'L.8.*_USGS_CFMASK.*', 3, 'USGS_CFMASK_CONF', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 681, 73, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 682, 73, 2);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_attr.tif'
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (691, 6, 'usgs sr cloud', 1, 10, 30, null, null, 'L.8.*_USGS_SR_ATTR.*', 3, 'USGS_SR_CLOUD', 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 691, 71, 1);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_band.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (701, 6, 'usgs sr band1', 1, 11, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND1', 1);
@@ -146,16 +176,22 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (705, 6, 'usgs sr band5', 1, 11, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND5', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (706, 6, 'usgs sr band6', 1, 11, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND6', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (707, 6, 'usgs sr band7', 1, 11, 30, null, null, 'L.8.*_USGS_SR_BAND.*', 3, 'USGS_SR_BAND7', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 701, 70, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 702, 70, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 703, 70, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 704, 70, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 705, 70, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 706, 70, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 707, 70, 7);
 -- Example: 'CONUS_3.2_1.0_LC80470272013303LGN00_sr_int16_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (711, 6, 'usgs sr evi', 1, 12, 30, null, null, 'L.8.*_USGS_SR_INT16_ATTR.*', 3, 'USGS_SR_EVI', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (712, 6, 'usgs sr ndvi', 1, 12, 30, null, null, 'L.8.*_USGS_SR_INT16_ATTR.*', 3, 'USGS_SR_NDVI', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 711, 72, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 712, 72, 2);
 
 
 -- Landsat 5 TM
@@ -163,7 +199,10 @@ insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6,
 -- Example: 'CONUS_3.2_1.0_LT50470272010247PAC01_cfmask.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (721, 2, 'usgs cfmask', 1, 13, 30, null, null, 'LT5.*_USGS_CFMASK.*', 1, 'USGS_CFMASK', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (722, 2, 'usgs cfmask conf', 1, 13, 30, null, null, 'LT5.*_USGS_CFMASK.*', 1, 'USGS_CFMASK_CONF', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 721, 73, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 722, 73, 2);
 -- Example: 'CONUS_3.2_1.0_LT50470272010247PAC01_sr_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (731, 2, 'usgs sr adjacent cloud qa', 1, 14, 30, null, null, 'LT5.*_USGS_SR_ATTR.*', 1, 'USGS_SR_ADJACENT_CLOUD_QA', 1);
@@ -177,12 +216,15 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (735, 2, 'usgs sr fill qa', 1, 14, 30, null, null, 'LT5.*_USGS_SR_ATTR.*', 1, 'USGS_SR_FILL_QA', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (736, 2, 'usgs sr land water qa', 1, 14, 30, null, null, 'LT5.*_USGS_SR_ATTR.*', 1, 'USGS_SR_LAND_WATER_QA', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (737, 2, 'usgs sr snow qa', 1, 14, 30, null, null, 'LT5.*_USGS_SR_ATTR.*', 1, 'USGS_SR_SNOW_QA', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 731, 71, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 732, 71, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 733, 71, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 734, 71, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 735, 71, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 736, 71, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 737, 71, 7);
 -- Example: 'CONUS_3.2_1.0_LT50470272010247PAC01_sr_band.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (741, 2, 'usgs sr atmos opacity', 1, 15, 30, null, null, 'LT5.*_USGS_SR_BAND.*', 1, 'USGS_SR_ATMOS_OPACITY', 1);
@@ -196,13 +238,19 @@ insert into band (band_id, sensor_id, band_name, band_type_id, file_number, reso
 values (745, 2, 'usgs sr band4', 1, 15, 30, null, null, 'LT5.*_USGS_SR_BAND.*', 1, 'USGS_SR_BAND4', 5);
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (746, 2, 'usgs sr band5', 1, 15, 30, null, null, 'LT5.*_USGS_SR_BAND.*', 1, 'USGS_SR_BAND5', 6);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (747, 2, 'usgs sr band7', 1, 15, 30, null, null, 'LT5.*_USGS_SR_BAND.*', 1, 'USGS_SR_BAND7', 7);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 741, 70, 1);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 742, 70, 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 743, 70, 3);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 744, 70, 4);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 745, 70, 5);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 746, 70, 6);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 747, 70, 7);
 -- Example: 'CONUS_3.2_1.0_LT50470272010247PAC01_sr_int16_attr.tif'
 insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (751, 2, 'usgs sr evi', 1, 16, 30, null, null, 'LT5.*_USGS_SR_INT16_ATTR.*', 1, 'USGS_SR_EVI', 1);
+insert into band (band_id, sensor_id, band_name, band_type_id, file_number, resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
+values (752, 2, 'usgs sr ndvi', 1, 16, 30, null, null, 'LT5.*_USGS_SR_INT16_ATTR.*', 1, 'USGS_SR_NDVI', 2);
 insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 751, 72, 1);
+insert into band_source (tile_type_id, band_id, level_id, tile_layer) values (6, 752, 72, 2);
